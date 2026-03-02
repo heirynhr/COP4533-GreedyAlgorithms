37b349fc8cb685ecd16cc6405e4862dd03d2a844
maitechapartegui <chaparteguimaite@ufl.edu>
2026-03-02 13:01:01 -0500

    Added OPTFF, haven't tested yet

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -88,5 +89,61 @@ struct Cache {
 				}
 			}
 		}
+	}
+
+	void doOPTFF(const vector<int>& allRequests){
+		// need to build list of each requests next use
+		// as we iterate through the requests we need to know the next time request x appears in the requests
+		// need to visualize this:
+			// allRequests = [1, 2, 1, 3]
+			// next_request = [2, 4, 4, 4] *use m to signify that item doesnt appear again in the requests
+			// so basically 1's next use is in index 2, and the rest of the items dont appear again
+
+		vector<int> next_request;
+		// iterate through requests in reverse
+		map<int, int> last_seen;
+		for(int i = allRequests.size() - 1; i >= 0; --i){
+			int curr = allRequests[i];
+
+			// check if this request has already been seen to the right
+			if(last_seen.count(curr) == 1){
+				// if item already exists as a key, this means it has been visited in the array already and exists in the future
+				// store requests future occurence
+				next_request[i] = last_seen[curr];
+			}
+			else{
+				last_seen[curr] = i;
+			}
+		}
+
+		// now we need to evict the item whose next req is furthertest in the future or never occurs again
+		// what if multiple never occur again...
+		
+		// perform actual algorithm now
+		// use map for easier lookup
+		map<int, int> cache;
+		for(int i = 0; i < allRequests.size(); ++i){
+			int curr = allRequests[i];
+
+			if(cache.count(curr) == 1){
+				// hit, item is currently in cache and need to update its next req value
+				cache[curr] = next_request[i];
+			}
+			else{
+				missCount++;
+				if(cache.size() < maxCapacity){
+					// point to the cache entry with the largest next use value
+					auto evict = max_element(cache.begin(), cache.end(),
+                        [](const pair<int,int>& a, const pair<int,int>& b) {
+                            return a.second < b.second;
+                		});
+
+						// remove item from cache
+						cache.erase(evict);
+						cache[curr] = next_request[i];
+				}
+			}
+		}
+
 	}
 };
